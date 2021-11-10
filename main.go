e1dfecb6831933e103302bfc101cd5b1817db60a
root <allenlo0130@gmail.com>
2021-11-10 09:13:42 +0000

    [mod] modify func:main for exec with multiple workers

--- main.go
+++ main.go
@@ -8,19 +8,51 @@ import (
 	"modules/requests"
 )
 
-func main() {
-	// load args
-	args := readers.GetArgs()
-	requestList := readers.ReadSettings(args.ConfigPath)
+type (
+	config = requests.RequestConfig	
+)
 
-	// perform tests
-	for i := 0; i < len(requestList.Cases); i++ {
-		r := requestList.Cases[i]
-		r.Url = requestList.Domain + requestList.Cases[i].Route
-		err := requests.PerformRequest(r)
+func worker(jobs <- chan requests.RequestConfig, results chan <- string) {
+	for job := range jobs {
+		err := requests.PerformRequest(job)
 		if err != nil {
-			errors.New("Error in" + r.Url + "...")
+			errors.New("Error in" + job.Url + "...")
+			results <- "Failed  " + err.Error()
+		} else {
+			results <- "Success " + job.Url
 		}
-		fmt.Println(err)
+	}
+}
+
+func main() {
+	// // load args
+	// args := readers.GetArgs()
+	// requestList := readers.ReadSettings(args.ConfigPath)
+	requestList := readers.ReadSettings("/source/docs/examples.json")
+
+	rounds := requestList.Rounds
+	workers := requestList.Workers
+
+	numJobs := len(requestList.Cases)
+	jobs := make(chan config, numJobs * rounds)
+	results := make(chan string, numJobs * rounds)
+
+	for w := 1; w <= workers; w++ {
+		go worker(jobs, results)
+	}
+
+	for i := 0; i < numJobs * rounds; i++ {
+		d := i % numJobs
+		r := requestList.Cases[d]
+		r.Url = requestList.Domain + requestList.Cases[d].Route
+
+		jobs <-r
+	}
+	close(jobs)
+	
+	for i := 0; i < numJobs * rounds; i++ {
+		// <-results
+		x := <-results
+		fmt.Println(x)
 	}
 }
