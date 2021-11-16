d1a13687b59eacd03f7f870a9585f54eb5291d64
root <allenlo0130@gmail.com>
2021-11-16 09:55:02 +0000

    [mod] add test:TestGetHeaders and test:TestGetValues

---
+++ modules/export/export_test.go
@@ -0,0 +1,108 @@
+/*
+to test it, do the following:
+	move the "modules" folder under the GOROOT
+	go test -v -cover modules/export
+*/
+
+package export
+
+import (
+	"testing"
+)
+
+func equalSlice(s1 []interface{}, s2 []interface{}) bool {
+	if len(s1) != len(s2) {	
+		return false
+	}
+	for i, v := range(s1) {
+		if v != s2[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func stringSliceToInterfaceSlice(s []string) []interface{} {
+	var rt []interface{}
+	for _, v := range s {
+		rt = append(rt, v)
+	}
+
+	return rt
+}
+
+func TestGetHeaders(t *testing.T) {
+	type Input struct {
+		X string
+		Y string
+	}
+	type Want []interface{}
+	cases := []struct {
+		input Input
+		want Want
+	}{
+		{
+			Input{
+				X: "abc",
+				Y: "abc",
+			},
+			Want{
+				"X",
+				"Y",
+			},
+		},
+	}	
+
+	for _, c := range cases {
+		rt := stringSliceToInterfaceSlice(getHeaders(c.input))
+		if !equalSlice(rt, c.want) {
+			t.Errorf("readers.getHeaders of (%s) was incorrect, got: %s, want: %s", c.input, rt, c.want)
+		}
+	}
+}
+
+func TestGetValues(t *testing.T) {
+	type Input struct {
+		X string
+		Y int
+		Z interface{}
+	}
+	type Want []interface{}
+	cases := []struct {
+		input Input
+		want Want
+	}{
+		{
+			Input{
+				X: "abc",
+				Y: 123,
+				Z: 123,
+			},
+			Want{
+				"abc",
+				123,
+				123,
+			},
+		},
+		{
+			Input{
+				X: "abc",
+				Y: 123,
+				Z: "cba",
+			},
+			Want{
+				"abc",
+				123,
+				"cba",
+			},
+		},
+	}	
+
+	for _, c := range cases {
+		rt := getValues(c.input)
+		if !equalSlice(rt, c.want) {
+			t.Errorf("readers.getValues of (%#v) was incorrect, got: %#v, want: %#v", c.input, rt, c.want)
+		}
+	}
+}
