d27a84dec7e440f0684cf5161c230988fb2bdf8d
root <allenlo0130@gmail.com>
2021-11-16 10:56:09 +0000

    [add] add test:TestReadSettings

--- modules/readers/readers_test.go
+++ modules/readers/readers_test.go
@@ -8,6 +8,9 @@ package readers
 
 import (
 	"testing"
+	"reflect"
+
+	"modules/requests"
 )
 
 func TestGetLocatedFolder(t *testing.T) {
@@ -24,5 +27,22 @@ func TestGetLocatedFolder(t *testing.T) {
 		if rt != c.want {
 			t.Errorf("readers.getLocatedFolder of (%s) was incorrect, got: %s, want: %s", c.input, rt, c.want)
 		}
+	}
+}
+
+func TestReadSettings(t *testing.T) {
+	var cfg requests.AppConfig
+	cfg.Domain = "http://172.17.0.2:80"
+	cfg.Rounds = 2
+	cfg.Workers = 2
+	var case1 requests.RequestConfig
+	case1.Route = "/route"
+	case1.RequestType = "GET"
+	case1.EstElapse = 200
+	cfg.Cases = append(cfg.Cases, case1)
+
+	r := ReadSettings("/source/docs/simpleExample.json")
+	if !reflect.DeepEqual(cfg, r) {
+		t.Errorf("readers.ReadSettings of (%#v) was incorrect, got: %#v, want: %#v", "/source/docs/simpleExample.json", r, cfg)
 	}
 }
