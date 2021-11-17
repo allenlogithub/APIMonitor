8c0f74ad2e818dd1a04af92b8f06c07f8a3bf08c
root <allenlo0130@gmail.com>
2021-11-17 09:40:22 +0000

    [add] add test:TestCSVWrapper

--- modules/export/export_test.go
+++ modules/export/export_test.go
@@ -8,6 +8,7 @@ package export
 
 import (
 	"testing"
+	"reflect"
 )
 
 func equalSlice(s1 []interface{}, s2 []interface{}) bool {
@@ -103,6 +104,71 @@ func TestGetValues(t *testing.T) {
 		rt := getValues(c.input)
 		if !equalSlice(rt, c.want) {
 			t.Errorf("export.getValues of (%#v) was incorrect, got: %#v, want: %#v", c.input, rt, c.want)
+		}
+	}
+}
+
+func TestCSVWrapper(t *testing.T) {
+	type data struct {
+		X interface{}
+		Y interface{}
+	}	
+	d1 := data{
+		X: "abc",
+		Y: "cba",
+	}
+	d2 := data{
+		X: "123",
+		Y: "321",
+	}
+	d3 := data{
+		X: "123",
+		Y: 123,
+	}
+	d4 := data{
+		X: true,
+		Y: 1.1,
+	}
+	cases := []struct {
+		input []interface{}
+		want csvStruct
+	}{
+		{
+			[]interface{}{
+				d1,
+				d2,
+			},
+			csvStruct{
+				Data: []interface{}{d1, d2},
+				Headers: []string{"X", "Y"},
+			},
+		},
+		{
+			[]interface{}{
+				d1,
+			},
+			csvStruct{
+				Data: []interface{}{d1},
+				Headers: []string{"X", "Y"},
+			},
+		},
+		{
+			[]interface{}{
+				d1,
+				d2,
+				d3,
+				d4,
+			},
+			csvStruct{
+				Data: []interface{}{d1, d2, d3, d4},
+				Headers: []string{"X", "Y"},
+			},
+		},
+	}
+	for _, c := range cases {
+		rt := CSVWrapper(&c.input)
+		if !reflect.DeepEqual(*rt, c.want) {
+			t.Errorf("export.getValues of (%#v) was incorrect, got: %#v, want: %#v", &c.input, *rt, c.want)
 		}
 	}
 }
